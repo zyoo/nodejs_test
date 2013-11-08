a4774a5b71bfce6868f6e9e14839c681a0ff8f67
zhouyang <zhouyang@miaozhen.com>
2013-11-08 18:11:57 +0800

    try commit

--- nodejs_test/app.js
+++ nodejs_test/app.js
@@ -6,9 +6,55 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var service=require('./routes/service');
 var http = require('http');
 var path = require('path');
 var ejs = require('ejs');
+
+var mysql1=require('mysql');
+var exec=require('child_process').exec;
+var conn1=mysql1.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'cluster_manager',
+    port:3306
+});
+
+function update_one_host_info(host,file_path,port)
+{
+    var api_gethostinfo_cmd='sh host -g '+host+' -p '+port+' -k '+file_path;
+    exec(api_gethostinfo_cmd,function(err,stdout,stderr){
+           if(err){
+                console.log(err); 
+           } 
+           else{
+                var value=stdout['load'];         
+                conn1.query('update host_info set load=? where host=?',[value,host],function(err,result){
+                    if(err){
+                        console.log(err); 
+                    }
+                }); 
+           }
+    }); 
+}
+function update_host_info()
+{
+    conn1.query('select host,port,file_path from host_info',function(err,result){
+        if(err){
+            console.log(err); 
+        }
+        else{
+            for(var i=0;i<result.length;i++){
+                update_one_host_info(result[i]['host'],result[i]['file_path'],result[i]['port']); 
+            }
+        }
+            
+    });    
+}
+
+// update host info table every a minute
+//setInterval(update_host_info,60*1000);
 
 var app = express();
 
@@ -37,7 +83,9 @@ app.post('/login',routes.doLogin);
 app.get('/logout',routes.logout);
 app.get('/home',routes.home);
 app.get('/server',routes.server);
-app.get('/service',routes.service);
+app.get('/service',service.show_service);
+app.post('/service/select_service',service.select_service);
+app.post('/service/select_hosts_for_service',service.select_hosts_for_service);
 app.post('/server/add_server_check',routes.add_server_first);
 app.post('/server/add_server_add',routes.add_server_second);
 
--- nodejs_test/routes/index.js
+++ nodejs_test/routes/index.js
@@ -3,6 +3,7 @@
  * GET home page.
  */
 var mysql=require('mysql');
+var exec=require('child_process').exec;
 var conn=mysql.createConnection({
     host:'localhost',
     user:'root',
@@ -43,27 +44,50 @@ exports.home=function(req,res){
 }
 exports.server=function(req,res){
     conn.query("select * from host_info",function(err,result){
-        if(err){
-            console.log(err);
-            res.render('server',{title:'Server:Add New Server',flag:'false',CheckRes:'database option failed',DataBase_Res:''});
-        }
-        else{
-            console.log(typeof result);
-            var str_result=JSON.stringify(result);
-            console.log(str_result);
-/*                for(var i=0;i<result.length();i++){
-                console.log('\n');
-                console.log(result[i]);
+            if(err){
+                console.log(err);
+                res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:'database option failed',DataBase_Res:''});
             }
-*/
-            var str_result1=str_result.replace(/"/g,'\"');
-            res.render('server',{title:'Server',flag:'false',CheckRes:'',DataBase_Res:str_result1});
-        }
+            else{
+                var count=0;
+                for(var i=0;i<result.length;i++){
+                    if(result[i]['host'] == ''){
+                        console.log(result[i]);
+                        continue;
+                    }
+                    console.log(result[i]['host']);
+                    conn.query("select host,role_name from services_on_the_hosts_info where host='"+result[i]['host']+"'",function(host_number,total_info){
+                        return function(err,result){
+                            count++;
+                            if(err){
+                                console.log(err);
+                            }
+                            else{
+                                console.log(result); 
+                                console.log(typeof result);
+                                if(result.length == 0)
+                                    total_info[host_number]['role_name']='';
+                                else
+                                    total_info[host_number]['role_name']=result[0]['role_name'];
+                                for(var j=1;j<result.length;j++){
+                                    total_info[host_number]['role_name']=total_info[host_number]['role_name']+','+result[j]['role_name'];
+                                }
+                                console.log(total_info[host_number]);
+                                if(count ==total_info.length){
+                                    var str_result=JSON.stringify(total_info);
+                                    var str_result1=str_result.replace(/"/g,'\"');
+                                    res.render('server',{title:'Server',flag:'false',CheckRes:'',DataBase_Res:str_result1});
+
+                                }
+                            }
+                        };
+                    }(i,result) );
+                }
+
+            }
     });
 }
-exports.service=function(req,res){
-    res.render('service',{title:'Service'});
-}
+
 exports.add_server_first=function(req,res){
     var host=req.body.machine_host;
     var port=req.body.machine_port;
@@ -74,23 +98,35 @@ exports.add_server_first=function(req,res){
         console.log("the machine host and port is "+host+":"+port);
         console.log(req.body);
        //here is the code which check the host is illegal or not. 
-        var illegal_flag=1;
-       if(illegal_flag){
-            conn.query("insert host_info set host=?,ip=?,cpu=?,ram=?,disk=?,os=?,load_info=?",[host,'192.168.1.222','16','2g','32g','centos5','20%'],function(err,result){
-                if(err){
-                    console.log('the err is \n');
-                    console.log(typeof err);
-                    res.render('server',{title:'Service:Add New Server',flag:'true',CheckRes:'update the database failed',DataBase_Res:''});
-                }
+        var api_checkhost_cmd='sh check '+host;
+        exec(api_checkhost_cmd,function(err,stdout,stderr){
+                if(err == null){
+                // get the base info of the host from yanqiang's api name:host -a:add server;-g get info.
+                var api_gethostinfo_cmd='sh host -g '+host+'-p '+port+'-k '+file;
+                exec(api_gethostinfo_cmd,function(err,stdout,stderr){
+                    if(err){
+                        res.render('server',{title:'Server:Add new server',flag:'true',CheckRes:'get the host info error',DataBase_Res:''}); 
+                    }
+                    else{
+                    //here process the api result
+                    conn.query("insert host_info set host=?,ip=?,cpu=?,ram=?,disk=?,os=?,load_info=?",[host,'192.168.1.222','16','2g','32g','centos5','20%'],function(err,result){
+                        if(err){
+                            console.log('the err is \n');
+                            console.log(typeof err);
+                            res.render('server',{title:'Service:Add New Server',flag:'true',CheckRes:'update the database failed',DataBase_Res:''});
+                            }
+                        else{
+                            res.render('server',{title:'Service:Add New Server',flag:'true',CheckRes:'the host is legal,please go on to add the server to the cluser',DataBase_Res:''});
+                        //call the api from xiaoqiang to install the nagios and other base service
+                            }
+                        });                   
+                    } 
+                    });
+                 }
                 else{
-                    res.render('server',{title:'Service:Add New Server',flag:'true',CheckRes:'the host is legal,please go on to add the server to the cluser',DataBase_Res:''});
-                    //call the api from xiaoqiang to install the nagios and other base service
-                }
-            });
-        }
-        else{
-            res.render('server',{title:"Server:Add New Server",CheckRes:'the input info is illegal',DataBase_Res:''});
-        }
+                    res.render('server',{title:"Server:Add New Server",CheckRes:'the input info is illegal',DataBase_Res:''});
+                }               
+        });
     }
 }
 
@@ -102,23 +138,56 @@ exports.add_server_second=function(req,res){
     var but_value=req.body.SecondNext;
     if(but_value=="true"){
         //call the api from xiaoqiang to install the nagios and other base service        
-        conn.query("select * from host_info",function(err,result){
+        var api_addhost_cmd='sh host -a '+host+'-p '+port+'-k '+file;
+        exec(api_addhost_cmd,function(err,stdout,stderr){
             if(err){
-                console.log(err);
-                res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:'database option failed',DataBase_Res:''});
+                res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:stdout,DataBase_Res:''});
             }
             else{
-                console.log(typeof result);
-                var str_result=JSON.stringify(result);
-                console.log(str_result);
-/*                for(var i=0;i<result.length();i++){
-                    console.log('\n');
-                    console.log(result[i]);
-                }
-*/
-                var str_result1=str_result.replace(/"/g,'\"');
-                res.render('server',{title:'Server',flag:'false',CheckRes:'',DataBase_Res:str_result1});
-            }
+                conn.query("select * from host_info",function(err,result){
+                    if(err){
+                        console.log(err);
+                        res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:'database option failed',DataBase_Res:''});
+                    }
+                    else{
+                        var count=0;
+                        for(var i=0;i<result.length;i++){
+                            if(result[i]['host'] == ''){
+                                console.log(result[i]);
+                                continue;
+                            }
+                            console.log(result[i]['host']);
+                            conn.query("select host,role_name from services_on_the_hosts_info where host='"+result[i]['host']+"'",function(host_number,total_info){
+                                return function(err,result){
+                                    count++;
+                                    if(err){
+                                        console.log(err);
+                                    }
+                                    else{
+                                        console.log(result); 
+                                        console.log(typeof result);
+                                        if(result.length == 0)
+                                            total_info[host_number]['role_name']='';
+                                        else
+                                            total_info[host_number]['role_name']=result[0]['role_name'];
+                                        for(var j=1;j<result.length;j++){
+                                            total_info[host_number]['role_name']=total_info[host_number]['role_name']+','+result[j]['role_name'];
+                                        }
+                                        console.log(total_info[host_number]);
+                                        if(count ==total_info.length){
+                                            var str_result=JSON.stringify(total_info);
+                                            var str_result1=str_result.replace(/"/g,'\"');
+                                            res.render('server',{title:'Server',flag:'false',CheckRes:'',DataBase_Res:str_result1});
+
+                                        }
+                                   }
+                                };
+                            }(i,result) );
+                        }
+               
+                    }
+                });
+            } 
         });
     }
 }
