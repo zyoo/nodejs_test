2eec05212a3fe781e675a254106626a52aa9bad9
zhouyang <zhouyang@miaozhen.com>
2013-11-11 18:41:45 +0800

    hello

--- nodejs_test/routes/index.js
+++ nodejs_test/routes/index.js
@@ -2,6 +2,7 @@
 /*
  * GET home page.
  */
+var fs=require('fs');
 var mysql=require('mysql');
 var exec=require('child_process').exec;
 var conn=mysql.createConnection({
@@ -43,19 +44,19 @@ exports.home=function(req,res){
     res.render('home',{title:'Home',user:user});
 }
 exports.server=function(req,res){
-    conn.query("select * from host_info",function(err,result){
+    conn.query("select host,ip,cpu,ram,disk,os,load_info,status from host_info",function(err,result){
             if(err){
                 console.log(err);
-                res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:'database option failed',DataBase_Res:''});
+                res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:'database option failed',DataBase_Res:'',host_arg:''});
             }
             else{
                 var count=0;
                 for(var i=0;i<result.length;i++){
                     if(result[i]['host'] == ''){
-                        console.log(result[i]);
+//                        console.log(result[i]);
                         continue;
                     }
-                    console.log(result[i]['host']);
+ //                   console.log(result[i]['host']);
                     conn.query("select host,role_name from services_on_the_hosts_info where host='"+result[i]['host']+"'",function(host_number,total_info){
                         return function(err,result){
                             count++;
@@ -76,7 +77,7 @@ exports.server=function(req,res){
                                 if(count ==total_info.length){
                                     var str_result=JSON.stringify(total_info);
                                     var str_result1=str_result.replace(/"/g,'\"');
-                                    res.render('server',{title:'Server',flag:'false',CheckRes:'',DataBase_Res:str_result1});
+                                    res.render('server',{title:'Server',flag:'false',CheckRes:'',DataBase_Res:str_result1,host_arg:''});
 
                                 }
                             }
@@ -91,40 +92,82 @@ exports.server=function(req,res){
 exports.add_server_first=function(req,res){
     var host=req.body.machine_host;
     var port=req.body.machine_port;
-    var file=req.body.files;
-    if(host == null || port == null || file == null)
+    var file=req.body.keyfile;
+    console.log(req.body);
+    console.log(JSON.stringify(req.body));
+    req.session.machine_host=host;
+    req.session.machine_port=port;
+//    console.log(req.files.files[0]);
+    if(host == null || port == null){
+        console.log("hello world");
         res.render('server',{title:'Service:Add New Server',flag:'true',CheckRes:'the input info is illegal',DataBase_Res:''});
+    }
     else{
+        var current_path=process.cwd();
+        var new_path=current_path+'/keyfile_dir/'+host+'.keyfile.list';
+        console.log(new_path);
+//        console.log(file);
+        fs.renameSync(req.files.keyfile.path,new_path);
+        fs.chmodSync(new_path,'600');
         console.log("the machine host and port is "+host+":"+port);
-        console.log(req.body);
        //here is the code which check the host is illegal or not. 
-        var api_checkhost_cmd='sh check '+host;
+        var api_checkhost_cmd='ping -c 5 '+host;
         exec(api_checkhost_cmd,function(err,stdout,stderr){
                 if(err == null){
-                // get the base info of the host from yanqiang's api name:host -a:add server;-g get info.
-                var api_gethostinfo_cmd='sh host -g '+host+'-p '+port+'-k '+file;
-                exec(api_gethostinfo_cmd,function(err,stdout,stderr){
-                    if(err){
-                        res.render('server',{title:'Server:Add new server',flag:'true',CheckRes:'get the host info error',DataBase_Res:''}); 
-                    }
-                    else{
-                    //here process the api result
-                    conn.query("insert host_info set host=?,ip=?,cpu=?,ram=?,disk=?,os=?,load_info=?",[host,'192.168.1.222','16','2g','32g','centos5','20%'],function(err,result){
+                    // get the base info of the host from yanqiang's api name:host -a:add server;-g get info.
+                    console.log(api_checkhost_cmd+' success');
+                    var api_gethostinfo_cmd='sh HOST -g '+host+' -p '+port+' -k '+new_path;
+                    exec(api_gethostinfo_cmd,function(err,stdout,stderr){
                         if(err){
-                            console.log('the err is \n');
-                            console.log(typeof err);
-                            res.render('server',{title:'Service:Add New Server',flag:'true',CheckRes:'update the database failed',DataBase_Res:''});
-                            }
+				console.log('api_gethostinfo_cmd failed');
+				console.log(err);
+                            res.render('server',{title:'Server:Add new server',flag:'true',CheckRes:'get the host info error:'+stdout+'.please cancle the operation',DataBase_Res:'',host_arg:''}); 
+                        }
                         else{
-                            res.render('server',{title:'Service:Add New Server',flag:'true',CheckRes:'the host is legal,please go on to add the server to the cluser',DataBase_Res:''});
-                        //call the api from xiaoqiang to install the nagios and other base service
+			    console.log(api_gethostinfo_cmd+' success');
+                            var host_info_res=stdout;
+                            var arr_host_info_res=new Array();
+                            arr_host_info_res=host_info_res.split(";");
+                            var list_host_info_res=[];
+                            for(var m=0;m<arr_host_info_res.length;m++){
+                                var temp_arr=new Array();
+//				console.log(arr_host_info_res[m]);
+                                temp_arr=(arr_host_info_res[m]).split(":");
+				if(temp_arr[1].indexOf('\n') != -1)
+					temp_arr[1]=temp_arr[1].substr(0,temp_arr[1].indexOf('\n'));
+                                list_host_info_res[temp_arr[0]]=temp_arr[1];
+				console.log(temp_arr[0]);
+//				console.log(temp_arr[1]);
+//				console.log(list_host_info_res[temp_arr[0]]);
                             }
-                        });                   
-                    } 
+                            //here process the api result
+//                            conn.query("delete from host_info_temp where host=?;insert into host_info_temp set host=?,ip=?,cpu=?,ram=?,disk=?,os=?,load_info=?",[host,host,list_host_info_res['ip'],list_host_info_res['cpu'],list_host_info_res['ram'],list_host_info_res['disk'],list_host_info_res['os'],list_host_info_res['load_info']],function(err,result){
+                            conn.query("delete from host_info_temp where host=?",[host],function(err,result){
+				if(err){
+
+				}
+				else{
+				    conn.query("insert into host_info_temp set host=?,ip=?,cpu=?,ram=?,disk=?,os=?,load_info=?,key_file_path=?,port=?,status=?",[host,'xxx.xxx.xxx.xxx',list_host_info_res['cpu'],list_host_info_res['ram'],list_host_info_res['disk'],'centos5','20%',new_path,port,'live'],function(err,result){
+					if(err){
+					    console.log('the err is \n');
+					    console.log( err);
+					    res.render('server',{title:'Service:Add New Server',flag:'true',CheckRes:'update the database failed',DataBase_Res:'',host_arg:''});
+					}
+					else{
+					    
+					    res.render('server',{title:'Service:Add New Server',flag:'true',CheckRes:'the host is legal,please go on to add the server to the cluser',DataBase_Res:'',host_arg:host});
+					//call the api from xiaoqiang to install the nagios and other base service
+					}
+				    }); 
+
+				}
+			    });
+                        } 
                     });
-                 }
+                }
                 else{
-                    res.render('server',{title:"Server:Add New Server",CheckRes:'the input info is illegal',DataBase_Res:''});
+			console.log(err);
+                    res.render('server',{title:"Server:Add New Server",flag:'true',CheckRes:'the input info for the host is illegal,the check err is '+JSON.stringify(err)+stdout+'. please check the host info,then try again',DataBase_Res:'',host_arg:''});
                 }               
         });
     }
@@ -132,61 +175,90 @@ exports.add_server_first=function(req,res){
 
 //function install_base_service(arg1,arg2,callback)
 exports.add_server_second=function(req,res){
-    var host=req.body.machine_host;
-    var port=req.body.machine_port;
-    var file=req.body.files;
+//    var host=req.body.machine_host1;
+//    var port=req.body.machine_port1;
+    var host=req.session.machine_host;
+    var port=req.session.machine_port;
     var but_value=req.body.SecondNext;
+
     if(but_value=="true"){
         //call the api from xiaoqiang to install the nagios and other base service        
-        var api_addhost_cmd='sh host -a '+host+'-p '+port+'-k '+file;
+        var current_path=process.cwd();
+        var new_path=current_path+'/keyfile_dir/'+host+'.keyfile.list';
+        var api_addhost_cmd='sh HOST -a '+host+' -p '+port+' -k '+new_path;
+	console.log(api_addhost_cmd);
         exec(api_addhost_cmd,function(err,stdout,stderr){
             if(err){
-                res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:stdout,DataBase_Res:''});
+		console.log('api_addhost_cmd failed');
+		console.log(err);
+                res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:'add host failed,the error info is '+JSON.stringify(err)+stdout,DataBase_Res:'',host_arg:''});
             }
             else{
-                conn.query("select * from host_info",function(err,result){
-                    if(err){
-                        console.log(err);
-                        res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:'database option failed',DataBase_Res:''});
-                    }
-                    else{
-                        var count=0;
-                        for(var i=0;i<result.length;i++){
-                            if(result[i]['host'] == ''){
-                                console.log(result[i]);
-                                continue;
-                            }
-                            console.log(result[i]['host']);
-                            conn.query("select host,role_name from services_on_the_hosts_info where host='"+result[i]['host']+"'",function(host_number,total_info){
-                                return function(err,result){
-                                    count++;
-                                    if(err){
-                                        console.log(err);
-                                    }
-                                    else{
-                                        console.log(result); 
-                                        console.log(typeof result);
-                                        if(result.length == 0)
-                                            total_info[host_number]['role_name']='';
-                                        else
-                                            total_info[host_number]['role_name']=result[0]['role_name'];
-                                        for(var j=1;j<result.length;j++){
-                                            total_info[host_number]['role_name']=total_info[host_number]['role_name']+','+result[j]['role_name'];
-                                        }
-                                        console.log(total_info[host_number]);
-                                        if(count ==total_info.length){
-                                            var str_result=JSON.stringify(total_info);
-                                            var str_result1=str_result.replace(/"/g,'\"');
-                                            res.render('server',{title:'Server',flag:'false',CheckRes:'',DataBase_Res:str_result1});
+		console.log(api_addhost_cmd+' success');
+//                var result=stdout;
+ //               var arr_info=result.split(";");
+                conn.query("insert into host_info select * from host_info_temp where host=?",[host],function(err,result){
+			if(err){
+				console.log("insert host_info from host_info_temp failed");
+				console.log(err);
+				res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:'add host failed,the error info is '+JSON.stringify(err),DataBase_Res:'',host_arg:''});
+			}
+			else{
+				conn.query("delete from host_info_temp where host=?",[host],function(err,result){
+				     if(err){
+					 res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:'add host failed,the error info is '+JSON.stringify(err)+stdout,DataBase_Res:'',host_arg:''});
+				     }
+				     else{
+					 conn.query("select host,ip,cpu,ram,disk,os,load_info,status from host_info",function(err,result){
+					    if(err){
+						console.log(err);
+						res.render('server',{title:'Server:Add New Server',flag:'true',CheckRes:'database option failed',DataBase_Res:'',host_arg:''});
+					    }
+					    else{
+						var count=0;
+						for(var i=0;i<result.length;i++){
+						    if(result[i]['host'] == ''){
+							console.log(result[i]);
+							continue;
+						    }
+						    console.log(result[i]['host']);
+						    conn.query("select host,role_name from services_on_the_hosts_info where host=?",[result[i]['host']],function(host_number,total_info){
+							return function(err,result){
+							    count++;
+							    if(err){
+								console.log(err);
+							    }
+							    else{
+								console.log(result); 
+								console.log(typeof result);
+								if(result.length == 0)
+								    total_info[host_number]['role_name']='';
+								else
+								    total_info[host_number]['role_name']=result[0]['role_name'];
+								for(var j=1;j<result.length;j++){
+								    total_info[host_number]['role_name']=total_info[host_number]['role_name']+','+result[j]['role_name'];
+								}
+								console.log(total_info[host_number]);
+								if(count ==total_info.length){
+								    var str_result=JSON.stringify(total_info);
+								    var str_result1=str_result.replace(/"/g,'\"');
+									console.log(str_result1);
+								    res.render('server',{title:'Server',flag:'false',CheckRes:'',DataBase_Res:str_result1,host_arg:''});
 
-                                        }
-                                   }
-                                };
-                            }(i,result) );
-                        }
-               
-                    }
-                });
+								}
+							   }
+							};
+						    }(i,result) );
+						}
+				       
+					    }
+					});                   
+				    }
+				});
+			}
+		});
+
+
             } 
         });
     }
--- nodejs_test/routes/service.js
+++ nodejs_test/routes/service.js
@@ -9,7 +9,7 @@ var conn=mysql.createConnection({
 });
 exports.show_service=function(req,res){
     console.log('the request is '+req.body);
-    res.render('service',{title:'Service',Flag:'unset',Components:'',HostList:''});
+    res.render('service',{title:'Service',Flag:'unset',Components:'',HostList:'',Unique_Flag:'',Config:'',err_info:''});
 }
 
 exports.select_service=function(req,res){
@@ -23,25 +23,155 @@ exports.select_service=function(req,res){
         }
         else{
             var role_name=result[0]['role_name'];
-            conn.query('select host from host_info',function(err,result){
+            req.session.serviceChoice=serviceChoice;
+
+             conn.query('select host from host_info',function(err,result){
                 if(err){
                     console.log(err); 
+                    res.render('service',{title:'Service:Select Service',Flag:'false',Components:'',HostList:'',Unique_Flag:'',Config:'',err_info:'select host failed'});//should be modified
                 }      
                 else{
                     var str_host_list=JSON.stringify(result);
                     console.log(result);
                     console.log(str_host_list);
                       
-                    res.render('service',{title:'Service:Select Service',Flag:'true',Components:role_name,HostList:str_host_list});
+                    conn.query('select role_name,unique_flag from role_unique where service_name='+'"'+serviceChoice+'"',function(err,result){
+                        if(err){
+                            console.log(err); 
+                        }
+                        else{
+                            var str_res=JSON.stringify(result); 
+                            res.render('service',{title:'Service:Select Service',Flag:'true',Components:role_name,HostList:str_host_list,Unique_Flag:str_res,Config:'',err_info:''});
+                        }
+                        
+                    });
                 } 
                 
-            });
+            });                   
+
         }
         
     });
 }
 exports.select_hosts_for_service=function(req,res){
+    var service=req.session.serviceChoice;
+    var count=0;
+    var count_flag=0;
     console.log(req.body);
-    console.log(req.body.hostslist);
-    res.render('service',{title:'Service:Select Service',Flag:'unset',Components:'',HostList:''});
+    console.log(JSON.stringify(req.body)); 
+    conn.query('select role_name from services_can_be_installed where service_name=?',[service],function(err,result){
+            if(err){
+                console.log(err); 
+            }
+            else{
+                var role_name_arr=(result[0]['role_name']).split(",");
+                for(var j=0;j<role_name_arr.length;j++){
+                    var temp=req.body['hostslist_'+role_name_arr[j]]; 
+                    if(typeof temp == 'string'){
+                        count_flag++;
+                        console.log(temp);
+                        conn.query('insert into services_on_the_hosts_info_temp set host=?,role_name=?',[arr_host[0],arr_host[1]],function(err,result){
+                            if(err){
+                                console.log(err);
+                                res.render('service',{title:'Service:Select Service',Flag:'false',Components:'',HostList:'',Unique_Flag:'',Config:'',err_info:'insert into services_on_the_hosts_info_temp error'});
+                            } 
+                            else{
+                                count++;
+                                if(count==count_flag){
+                                //return the config page to modify the service's config
+                                    conn.query('select service_name,role_name,default_config from services_can_be_installed where service_name=?',[service],function(err,result){
+                                        if(err){
+                                        console.log(err); 
+                                        res.render('service',{title:'Service:Select Service',Flag:'false',Components:'',HostList:'',Unique_Flag:'',Config:'',err_info:'get default_config failed'});
+                                        }
+                                        else{
+                                        var str_config=JSON.stringify(result);
+                                        res.render('service',{title:'Service:Select Service',Flag:'true',Components:'',HostList:'',Unique_Flag:'',Config:str_config,err_info:''});
+                                        }
+                                    });
+                                }
+                            }
+                        });
+
+
+                    }
+
+                    else{
+                        for(var host_role in temp){
+                            count_flag++;
+                            var arr_host=new Array();
+                            arr_host=(temp[host_role]).split(',');
+                            conn.query('insert into services_on_the_hosts_info_temp set host=?,role_name=?',[arr_host[0],arr_host[1]],function(err,result){
+                                if(err){
+                                    console.log(err);
+                                    res.render('service',{title:'Service:Select Service',Flag:'false',Components:'',HostList:'',Unique_Flag:'',Config:'',err_info:'insert into services_on_the_hosts_info_temp error'});
+                                } 
+                                else{
+                                    count++;
+                                    if(count==count_flag){
+                                    //return the config page to modify the service's config
+                                        conn.query('select service_name,role_name,default_config from services_can_be_installed where service_name=?',[service],function(err,result){
+                                            if(err){
+                                            console.log(err); 
+                                            res.render('service',{title:'Service:Select Service',Flag:'false',Components:'',HostList:'',Unique_Flag:'',Config:'',err_info:'get default_config failed'});
+                                            }
+                                            else{
+                                            var str_config=JSON.stringify(result);
+                                            res.render('service',{title:'Service:Select Service',Flag:'true',Components:'',HostList:'',Unique_Flag:'',Config:str_config,err_info:''});
+                                            }
+                                        });
+                                    }
+                                }
+                            });
+                        }                   
+                    }
+                }
+            } 
+            
+    });
+    
+}
+exports.modify_config=function(req,res){
+    console.log(req.body);
+    var service=req.session.serviceChoice;
+    //var service='hdfs';
+    console.log(service);
+    conn.query('select current_config from services_on_running where service_name=?',[service],function(err,result){
+        if(err){
+            console.log(err); 
+        }
+        else{
+            var update_config='';
+            var config=result[0]['current_config'];
+            var role_config_arr=config.split(";");
+            for(var i=0;i<role_config_arr.length;i++){
+                if(role_config_arr[i].length == 0)
+                    continue;
+                var role_config=(role_config_arr[i]).split(":")[1];
+                var role_name=(role_config_arr[i]).split(":")[0];
+                update_config+=role_name+":";
+                var specific_config_arr=role_config.split(",");
+                for(var j=0;j<specific_config_arr.length;j++){
+                     if(specific_config_arr[j].length == 0)
+                        continue;
+                     var specific_config_name=(specific_config_arr[j]).split("^")[0];
+                     var specific_config_value=req.body[specific_config_name];
+                     update_config+=specific_config_name+'^'+specific_config_value+',';
+                }
+                update_config+=";";
+            }
+            console.log(update_config);
+            conn.query('update services_on_running set current_config=? where service_name=?',[update_config,service],function(err,result){
+                if(err){
+                    console.log(err);
+                    res.render('service',{title:'Service:Select Service',Flag:'false',Components:'',HostList:'',Unique_Flag:'',Config:'',err_info:'modify config failed'});
+                }         
+                else{
+                    res.render('service',{title:'Service:Select Service',Flag:'true',Components:'',HostList:'',Unique_Flag:'',Config:'',err_info:''});
+                }
+            });
+        }
+            
+    }); 
+
 }
